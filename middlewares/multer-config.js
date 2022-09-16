bb2fd3fe436d4ad7cf3b43529f56a180148965c4
Hoggy06 <speedrun.fra@gmail.com>
2022-09-16 21:18:01 +0200

    Changement de l'extension des images + limitation de poids image

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -10,17 +10,47 @@ const MIME_TYPES = {
 //Dossier de destination
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "images");
+    const pathRoute = req.originalUrl;
+    if (pathRoute === `/api/users/${req.params.id}`)
+      callback(null, "images/user");
+    if (pathRoute === "/api/posts/new") callback(null, "images/post");
+    if (pathRoute === `/api/posts/${req.params.id}`)
+      callback(null, "images/post");
   },
   //Formats du fichiers dans son appelation
   filename: (req, file, callback) => {
+    const pathRoute = req.originalUrl;
     //On supprime l'extension de fichier
-    const name = file.originalname.slice(0, -4);
+    const nameUserId = req.params.id;
+    const namePostTitle = req.body.title.split(" ").join("-");
     //On vérifie si l'extension est acceptée
     const extension = MIME_TYPES[file.mimetype];
     //photo_date.extension
-    callback(null, name.concat("_") + Date.now() + "." + extension);
+    pathRoute === `/api/users/${req.params.id}`
+      ? callback(null, nameUserId + "." + extension)
+      : callback(null, namePostTitle + "." + extension);
   },
 });
+
+const upload = multer({
+  fileFilter: (req, file, cb) => {
+    /* on récupère l'extension de notre image et 
+    on applique une regex pour définir les extensions que l'on souhaite
+    accepter au sein du middleware */
+    const ext = file.mimetype.split("/")[1];
+    const regExt = /^png|jpg|jpeg$/;
+    if (regExt.test(ext)) cb(null, true);
+    if (!regExt.test(ext)) cb({ message: "Type de fichier non accepté" });
+  },
+
+  // on impose une limite de taille à l'image de 3mo
+  limits: { fileSize: 3145728 },
+
+  storage,
+  // image sera le champ dédié aux fichiers images dans le formData
+}).single("image");
 //Exportation du module
-module.exports = multer({ storage: storage }).single("image");
+module.exports = (req, res, next) => {
+  upload(req, res, (err) => (err ? res.status(400).json(err) : next()));
+};
+//module.exports = multer({ storage: storage }).single("image");
