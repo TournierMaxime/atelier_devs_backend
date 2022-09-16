20ae0ebbfbc69831996b2f1bca9c5e1cf66303e6
Hoggy06 <speedrun.fra@gmail.com>
2022-09-16 21:13:54 +0200

    ajout des regex de vérifications

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,50 +6,109 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 //const schemaPassword = require("../models/passwordValidator.js");
 const User = require("../models/Users.js")(sequelize, DataTypes);
+const Token = require("../models/Token.js")(sequelize, DataTypes);
 const fs = require("fs");
-const { encryptEmail } = require("../middlewares/crypto.js");
-const {
-  testFirstName,
-  testLastName,
-  testEmail,
-  testPassword,
-} = require("../functions/regex.js");
+const crypto = require("crypto");
+const { encryptEmail, decryptEmail } = require("../middlewares/crypto.js");
+const regex = require("../functions/regex.js");
+const emailSignup = require("../middlewares/emailSignup.js");
+const emailResetPassword = require("../middlewares/emailResetPassword.js");
+const moment = require("moment");
 
 //Fonction signup
 exports.signup = (req, res, next) => {
-  //Vérification des champs + regex
-  if (
-    req.body.firstname === "" &&
-    req.body.lastname === "" &&
-    req.body.email === "" &&
-    req.body.password === ""
-  ) {
-    return res.status(400).send({
-      error: `Tous les champs doivent etre complétés`,
-    });
-  }
+  User.findOne({
+    where: { email: req.body.email },
+  })
+    .then((user) => {
+      //Vérification des champs + regex
+      if (
+        req.body.firstname === "" &&
+        req.body.lastname === "" &&
+        req.body.email === "" &&
+        req.body.password === ""
+      ) {
+        return res.status(400).send({
+          error: `Tous les champs doivent etre complétés`,
+        });
+      }
 
-  testFirstName(req.body.firstname);
-  testLastName(req.body.lastname);
-  testEmail(req.body.email);
-  testPassword(req.body.password);
+      if (
+        regex.testFirstName(req.body.firstname) === false ||
+        req.body.firstname === ""
+      ) {
+        return res.status(400).send({
+          error: `Merci de vérifier votre prénom, 3 caractères minimum requis avec des lettres uniquement`,
+        });
+      }
 
-  //On rentre les différents champs en bdd
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      User.create({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: encryptEmail(req.body.email),
-        password: hash,
-        image: `http://localhost:${process.env.PORT_SERVER}/images/default.png`,
-        isAdmin: false,
-      })
-        .then(() =>
-          res.status(201).json({ message: "Compte crée avec succès" })
-        )
-        .catch(() => res.status(400).json({ error: "Email déjà utilisé" }));
+      if (
+        regex.testLastName(req.body.lastname) === false ||
+        req.body.lastname === ""
+      ) {
+        return res.status(400).send({
+          error: `Merci de vérifier votre nom, 3 caractères minimum requis avec des lettres uniquement`,
+        });
+      }
+
+      if (regex.testEmail(req.body.email) === false || req.body.email === "") {
+        return res.status(400).send({
+          error: `Merci de vérifier votre email, format invalide`,
+        });
+      }
+
+      if (
+        regex.testPassword(req.body.password) === false ||
+        req.body.password === ""
+      ) {
+        return res.status(400).send({
+          error: `Le mot de passe doit contenir au moins : 8 caractères minimum, une majuscule, une minuscule, un chiffre, et un caractère spécial`,
+        });
+      }
+      if (!user) {
+        //On rentre les différents champs en bdd
+        bcrypt.hash(req.body.password, 10).then((hash) => {
+          User.create({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: encryptEmail(req.body.email),
+            password: hash,
+            image: `http://localhost:${process.env.PORT_SERVER}/images/user/default.png`,
+            isConfirmed: false,
+            isAdmin: false,
+          })
+            .then((newUser) => {
+              emailSignup.sendMail(
+                decryptEmail(newUser.email),
+                emailSignup.confirmedAccount.confirm(newUser.id)
+              );
+            })
+            .then(() =>
+              res.status(201).json({ message: emailSignup.messages.confirm })
+            )
+            .catch(() =>
+              res.status(400).json({ error: "Veuillez vérifier vos champs" })
+            );
+        });
+      } // We have already seen this email address. But the user has not
+      // clicked on the confirmation link. Send another confirmation email.
+      else if (user && !user.isConfirmed) {
+        emailSignup
+          .sendEmail(
+            decryptEmail(user.email),
+            emailSignup.confirmedAccount.confirm(user.id)
+          )
+          .then(() =>
+            res.status(200).json({ message: emailSignup.messages.resend })
+          );
+      }
+
+      // The user has already confirmed this email address
+      else {
+        res
+          .status(200)
+          .json({ message: emailSignup.messages.alreadyConfirmed });
+      }
     })
     .catch((error) => res.status(500).json({ error }));
 };
@@ -58,7 +117,9 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
   //Recherche de User dans la bdd
   User.findOne({
-    where: { email: encryptEmail(req.body.email) },
+    where: {
+      email: encryptEmail(req.body.email),
+    },
   })
     //Si non trouvé 401
     .then((user) => {
@@ -66,6 +127,12 @@ exports.login = (req, res, next) => {
         return res
           .status(401)
           .send({ error: `Adresse mail ou mot de passe incorrect` });
+      }
+
+      if (user.isConfirmed === false) {
+        return res
+          .status(401)
+          .send({ error: `Votre compte n'est pas confirmé` });
       }
       //Utilisation de bcrypt pour la comparaison du mot de passe
       bcrypt
@@ -95,5 +162,145 @@ exports.login = (req, res, next) => {
     })
     //Gestion de l'erreur en 500 (server response)
     .catch((error) => res.status(500).json({ error }));
-  console.log(res);
+};
+
+// The callback that is invoked when the user visits the confirmation
+// url on the client and a fetch request is sent in componentDidMount.
+exports.confirmEmail = (req, res) => {
+  User.findOne({ where: { id: req.params.id } })
+    .then((user) => {
+      // A user with that id does not exist in the DB. Perhaps some tricky
+      // user tried to go to a different url than the one provided in the
+      // confirmation email.
+      if (!user) {
+        return res
+          .status(200)
+          .json({ message: emailSignup.messages.couldNotFind });
+      }
+
+      if (user.isConfirmed === true) {
+        return res
+          .status(200)
+          .json({ message: emailSignup.messages.alreadyConfirmed });
+      }
+
+      user
+        .update({ isConfirmed: true }, { where: { id: req.params.id } })
+        .then(() =>
+          res.status(200).json({ message: emailSignup.messages.confirmed })
+        )
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+};
+
+//Fonction login
+exports.emailResetPassword = (req, res, next) => {
+  //Recherche de User dans la bdd
+  User.findOne({
+    where: {
+      email: encryptEmail(req.body.email),
+    },
+  })
+    //Si non trouvé 401
+    .then((user) => {
+      if (!user) {
+        return res.status(401).send({ error: `Adresse mail incorrect` });
+      }
+
+      Token.create({
+        userId: user.id,
+        token: jwt.sign({ userId: user.id }, process.env.TOKEN_KEY, {
+          expiresIn: 3600,
+        }),
+      })
+        .then((token) => {
+          emailResetPassword.sendMail(
+            decryptEmail(user.email),
+            emailResetPassword.confirmedResetPassword.confirm(token.id)
+          );
+        })
+        .then(() =>
+          res.status(201).json({ message: emailResetPassword.messages.confirm })
+        );
+    })
+    //Gestion de l'erreur en 404 (server response)
+    .catch((error) => res.status(404).json({ error }));
+};
+
+//Recherche d'un utilisateur
+exports.getResetPassword = (req, res, next) => {
+  Token.findOne({
+    where: { id: req.params.id },
+  })
+    .then((token) => {
+      if (!token) {
+        return res.status(404).json({ error: "Aucune donnée" });
+      }
+      res.status(200).send({ message: "Ok" });
+    })
+    .catch(() => {
+      res.status(404).json({ error: "Aucune donnée" });
+    });
+};
+
+exports.resetPassword = (req, res, next) => {
+  Token.findOne({
+    where: { id: req.params.id },
+  })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(200)
+          .json({ message: emailResetPassword.messages.couldNotFind });
+      }
+      if (
+        regex.testPassword(req.body.password) === false ||
+        req.body.password === ""
+      ) {
+        return res.status(400).send({
+          error: `Le mot de passe doit contenir au moins : 8 caractères minimum, une majuscule, une minuscule, un chiffre, et un caractère spécial`,
+        });
+      }
+
+      if (moment().format() > moment(user.creation).add(1, "h").format()) {
+        Token.destroy({ where: { id: req.params.id } });
+        return res
+          .status(400)
+          .json({ message: emailResetPassword.messages.expire });
+      }
+
+      const setPassword = {
+        password: req.body.password,
+        confirmNewPassword: req.body.confirmNewPassword,
+      };
+
+      if (setPassword.password === setPassword.confirmNewPassword) {
+        bcrypt.hash(setPassword.password, 10).then((hash) => {
+          User.update(
+            {
+              password: hash,
+              id: req.params.id,
+            },
+            { where: { id: user.userId } }
+          )
+            .then(() => {
+              res.status(200).json({
+                message: emailResetPassword.messages.confirmed,
+              });
+              Token.destroy({ where: { id: req.params.id } });
+            })
+            .catch((error) => {
+              res.status(400).json({ error });
+            });
+        });
+      } else {
+        return res
+          .status(400)
+          .send({ error: `Les mots de passe ne correspondent pas` });
+      }
+    })
+    .catch(() => {
+      res.status(404).json({ error: "Utilisateur non trouvé" });
+    });
 };
