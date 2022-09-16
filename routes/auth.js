e392a8100000234e8b02ced0abe102a71a134ed6
Hoggy06 <speedrun.fra@gmail.com>
2022-09-16 21:18:54 +0200

    Ajout de nouvelles routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 //Controllers
-const userCtrl = require("../controllers/auth.js");
+const authCtrl = require("../controllers/auth.js");
 //Routes
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/signup", authCtrl.signup);
+router.post("/login", authCtrl.login);
+router.get("/confirm/:id", authCtrl.confirmEmail);
+router.post("/emailResetPassword", authCtrl.emailResetPassword);
+router.post("/resetPassword/:id", authCtrl.resetPassword);
+router.get("/resetPassword/:id", authCtrl.getResetPassword);
 
 module.exports = router;
