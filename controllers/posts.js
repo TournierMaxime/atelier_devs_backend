c0ba398adc3276687e624740759e295988b0702a
Hoggy06 <speedrun.fra@gmail.com>
2022-09-16 21:14:47 +0200

    ajout du titre pour les posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,8 +16,9 @@ exports.createPost = (req, res, next) => {
   //Si l'utilisateur décide d'importer un image
   const postObject = req.file
     ? {
+        title: req.body.title,
         message: req.body.message,
-        image: `${req.protocol}://${req.get("host")}/images/${
+        image: `${req.protocol}://${req.get("host")}/images/post/${
           req.file.filename
         }`,
       }
@@ -45,7 +46,7 @@ exports.getAllPosts = (req, res, next) => {
   //Utilisation des datas Posts et Users
   const options = {
     order: [["id", "DESC"]],
-    attributes: ["id", "message", "image", "userId", "created"],
+    attributes: ["id", "title", "message", "image", "userId", "created"],
     include: [
       {
         model: Users,
@@ -58,7 +59,7 @@ exports.getAllPosts = (req, res, next) => {
   };
   Posts.findAll(options)
     .then((posts) => {
-      res.status(200).json(posts);
+      res.status(200).json({ posts });
     })
     .catch(() => {
       res.status(404).json({ error: "Impossible de retrouver les posts" });
@@ -75,7 +76,7 @@ exports.getOnePost = (req, res, next) => {
   //Utilisation des datas Posts et Users
   const options = {
     where: { id: req.params.id },
-    attributes: ["id", "message", "image", "userId", "created"],
+    attributes: ["id", "title", "message", "image", "userId", "created"],
     include: [
       {
         model: Users,
@@ -111,8 +112,9 @@ exports.editPost = (req, res, next) => {
       //Si ajout d'un fichier
       const postObject = req.file
         ? {
+            title: req.body.title,
             message: req.body.message,
-            image: `${req.protocol}://${req.get("host")}/images/${
+            image: `${req.protocol}://${req.get("host")}/images/post/${
               req.file.filename
             }`,
           }
@@ -139,8 +141,8 @@ exports.deletePost = (req, res, next) => {
           }
           //Suppression de l'image dans le dossier images
           if (post.image) {
-            const filename = post.image.split("/images/")[1];
-            fs.unlink(`images/${filename}`, () => {
+            const filename = post.image.split("/images/post/")[1];
+            fs.unlink(`images/post/${filename}`, () => {
               //Suppression du post
               post.destroy();
               res.status(200).json({ message: "Post supprimé" });
@@ -161,12 +163,12 @@ exports.deletePost = (req, res, next) => {
 exports.deletePostImage = (req, res, next) => {
   Posts.findOne({ where: { id: req.params.id } })
     .then((post) => {
-      if (post.userId !== req.auth.userId && req.auth.isAdmin === false) {
+      if (post.userId !== req.auth.userId) {
         return res.status(403).json({ error: "Accès non autorisé" });
       }
 
-      const filename = post.image.split("/images/")[1];
-      fs.unlink(`images/${filename}`, () => {
+      const filename = post.image.split("/images/post/")[1];
+      fs.unlink(`images/post/${filename}`, () => {
         //Suppression du post
         post.update({ image: null });
         res.status(200).json({ message: "Image supprimée" });
